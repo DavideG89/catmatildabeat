afa22321d3fe9b91d258578bb6a3cecfa30d5602
Davide Giuliano <davidegiuliano89@gmail.com>
2025-10-15 11:21:18 +0200

    updates Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -123,9 +123,7 @@ export default function RootLayout({
                 function gtag(){dataLayer.push(arguments)}
                 gtag('js', new Date())
 
-                gtag('config', 'G-HG5MGWJS48', {
-                  page_path: window.location.pathname,
-                })
+                gtag('config', 'G-HG5MGWJS48')
               `}
             </Script>
           </>
